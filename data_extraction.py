86c60b91b894b5225d0607b570033bfb082ccc2e
sdelannoypavy <solene.delannoypavy_externe@rte-france.com>
2025-03-17 16:04:25 +0100

    Add files via upload

--- data_extraction.py
+++ data_extraction.py
@@ -3,12 +3,16 @@ import os
 os.environ['HTTP_PROXY'] = 'http://SD55089T:Pruneauprun01r1@163.104.40.34:3128'
 os.environ['HTTPS_PROXY'] = 'http://SD55089T:Pruneauprun01r1@163.104.40.34:3128'
 
-# La hauteur des vagues est plus significatives pour les conditions de maintenance
-# Pour les postes flottés, les contrats demandent que la plateforme soit accessible jusqu'à 1.5m - 2m de hauteur des vagues
+# Le paramètre significatif pour les conditions de maintenance est wind weight height
+# Pour les postes flottés, les contrats demandent que la plateforme soit accessible jusqu'à Hs = 1.5m
+# Pour Saint Nazaire, l'exigence a été baissée à 1m car tout le monde étatit alade à 1m
+# Un seuil de Hs (hauteur significative) correspond à un seuil de winfd speed
 # On regarde Significant height of combined wind waves and swell
-# Swell = grands régimes de vagues // wind waves 
+# Swell = grands régimes de vagues influencés par la gravité = swell // wind waves 
 
 import cdsapi
+
+# la lattitude et la longitude ont une résolution 0.5, donc poue avoir une seule donnée il faut un pas de 0.4
 
 dataset = "reanalysis-era5-single-levels"
 request = {
@@ -16,29 +20,13 @@ request = {
     "variable": [
         "significant_height_of_combined_wind_waves_and_swell"
     ],
-    "year": ["2015", "2016", "2017",
-        "2018", "2019", "2020",
-        "2021", "2022", "2023",
-        "2024", "2025"
+    "year": ["2017"
     ],
     "month": [
-        "01", "02", "03",
-        "04", "05", "06",
-        "07", "08", "09",
-        "10", "11", "12"
+        "01"
     ],
     "day": [
-        "01", "02", "03",
-        "04", "05", "06",
-        "07", "08", "09",
-        "10", "11", "12",
-        "13", "14", "15",
-        "16", "17", "18",
-        "19", "20", "21",
-        "22", "23", "24",
-        "25", "26", "27",
-        "28", "29", "30",
-        "31"
+        "01"
     ],
     "time": [
         "00:00", "01:00", "02:00",
@@ -51,9 +39,11 @@ request = {
         "21:00", "22:00", "23:00"
     ],
     "data_format": "netcdf",
-    "download_format": "zip",
-    "area": [-2.56, 49, -2.5605, 49.0005]
+    "download_format": "unarchived",
+    "area": [-2.56, 49, -2.16, 49.4]
 }
 
+target = 'data/fichier4.nc'
+
 client = cdsapi.Client()
-client.retrieve(dataset, request).download()
+client.retrieve(dataset, request, target)
